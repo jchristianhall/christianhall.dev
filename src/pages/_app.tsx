d0ccf5b6a4104946a7e75c4bcb96b4e4935bfdce
Christian Hall <jchristianhall@me.com>
2025-12-31 19:37:46 -0500

    chore: move from gosquared to posthog

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,24 @@
+import { useEffect } from 'react'
+import posthog from 'posthog-js'
+import { PostHogProvider } from 'posthog-js/react'
 import '../styles/global.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
+      api_host:
+        process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com',
+      person_profiles: 'identified_only',
+      defaults: '2025-11-30',
+      loaded: (posthog) => {
+        if (process.env.NODE_ENV === 'development') posthog.debug()
+      },
+    })
+  }, [])
+
+  return (
+    <PostHogProvider client={posthog}>
+      <Component {...pageProps} />
+    </PostHogProvider>
+  )
 }
